0e696146bd1db773e27509735bf3b176cc04c443
Laura Klünder <laura@codingcatgirl.de>
2017-10-03 11:16:54 +0200

    remove now unneeded split_syllables_de

--- haikufy.py
+++ haikufy.py
@@ -16,8 +16,7 @@ for c in string.ascii_lowercase+'öäüß':
     overrides_de[c] = 'yp-si-lon' if c == 'y' else c
 
 join_syllables_de = ('ti-on', 'ti-ons', 'nai-v', 'ge-ht')
-split_syllables_de = ('ei-nen', 'na-iv', 'de-o', 'de-os', 'pi-a', 'wa-rum', 'ü-ber', 'food-porn', 'en-tro',
-                      'o-dy')
+split_syllables_de = ('na-iv', 'de-o', 'de-os', 'pi-a', 'o-dy')
 
 
 def german_number_syllables(number):
