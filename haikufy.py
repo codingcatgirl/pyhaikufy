4219360646e6a68dd3fa7f1da15de4aa770c66ee
Laura Klünder <laura@codingcatgirl.de>
2017-10-06 16:27:21 +0200

    notebook

--- haikufy.py
+++ haikufy.py
@@ -49,8 +49,8 @@ replaces_de = {
 }
 
 join_syllables_de = ('ti-on', 'ti-ons', 'si-on', 'si-ons', 'nai-v', 'ge-ht', 'ed-ge', 'kin-ky', 'zi-ell', 'zi-el',
-                     'wi-ngs')
-split_syllables_de = ('na-iv', 'de-o', 'de-os', 'pi-a', 'o-dy', 'o-nym', 'o-ny', 'pro-xy')
+                     'wi-ngs', 'no-te-book')
+split_syllables_de = ('na-iv', 'de-o', 'de-os', 'pi-a', 'o-dy', 'o-nym', 'o-ny', 'pro-xy', 'note-book')
 no_syllable_start_de = ('bb', 'bc', 'bd', 'bp', 'cm', 'cn', 'cv', 'cw', 'cx',
                         'db', 'dc', 'df', 'dg', 'dh', 'dj', 'dk', 'dl', 'dm', 'dn', 'dp', 'dq', 'dz',
                         'gb', 'gc', 'gd', 'gf', 'gj', 'gk', 'gq'
